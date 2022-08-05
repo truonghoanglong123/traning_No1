a0703454e70a52a52f6de733079db64cfaa8ff62
unknown <truonghoanglongxyz@gmail.com>
2022-08-05 11:46:57 +0700

    fix menu

--- js/app.js
+++ js/app.js
@@ -4,10 +4,19 @@ var menu = document.querySelector(".section-menu")
 
 var fixed = document.querySelector('.fixed')
 window.addEventListener('scroll',function(){
-    console.log(window.pageYOffset)
+    // console.log(window.pageYOffset)
     if(window.pageYOffset > 480){  
         menu.classList.add('fixed')
     }else{
         menu.classList.remove('fixed')
     }
+})
+
+
+
+var button = document.querySelector('.nav__button')
+var nav = document.querySelector('.nav')
+
+button.addEventListener('click',function(){
+    console.log("-------")
 })
