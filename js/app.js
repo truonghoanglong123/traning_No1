8dfd42cc4858e737aae8ac977c4a019cf1d977b4
unknown <truonghoanglongxyz@gmail.com>
2022-08-08 14:13:57 +0700

    88

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var menu = document.querySelector(".section-menu")
 var fixed = document.querySelector('.fixed')
 window.addEventListener('scroll',function(){
     // console.log(window.pageYOffset)
-    if(window.pageYOffset > 490){  
+    if(window.pageYOffset > 480){  
         menu.classList.add('fixed')
     }else{
         menu.classList.remove('fixed')
