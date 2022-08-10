99d34693c963be978b95340fb8b0f8fec320d266
unknown <truonghoanglongxyz@gmail.com>
2022-08-10 17:19:30 +0700

    fixicon

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 var menu = document.querySelector(".section-menu")
 var fixed = document.querySelector('.fixed')
+var section_news = document.querySelector('.section-news')
 window.addEventListener('scroll',function(){
-    console.log(window.pageYOffset)
+    // console.log(window.pageYOffset)
     if(window.pageYOffset > 600){  
         menu.classList.add('fixed')
+        menu.classList.add('hideshow')
+        section_news.classList.add('gat')
+        
     }else{
         menu.classList.remove('fixed')
+        menu.classList.remove('hideshow')
+        section_news.classList.remove('gat')
     }
+})
+
+var button = document.querySelector('.nav__button')
+var icon = document.querySelector('.nav__button')
+button.addEventListener('click',function(){
+    icon.classList.toggle('chuyen')
 })
