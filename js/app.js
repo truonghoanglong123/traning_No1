795e517044adddf5811d5c46a3cf135c27988689
unknown <truonghoanglongxyz@gmail.com>
2022-08-10 10:05:59 +0700

    toggle menu

--- js/app.js
+++ js/app.js
@@ -11,8 +11,20 @@
 
 //////
 
+
 var button = document.querySelector('.nav__button')
-var body = document.querySelector('html')
+var body = document.querySelector('body')
+var nav = document.querySelector('.nav-menu')
 button.addEventListener('click',function(){
     body.classList.toggle('over');
+    body.classList.add('top');
+    nav.classList.toggle('displaymenu');
+    window.scrollTo(0,3000);
 })
+
+//////
+// function scrollWin() {
+//     body.scrollTo(0,(window.pageYOffset));
+// }
+// console.log(window.pageYOffset);
+// var top = window.pageYOffset;
